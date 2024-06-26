cb94a03b7d4703db7cd104f91763f1ed1ec8eab8
Anton <anton.zeltser@gmail.com>
2024-06-26 22:07:14 +0200

    Task-12: added logic: see the 3 latest user who liked one post

--- src/domain/like-for-posts-service.ts
+++ src/domain/like-for-posts-service.ts
@@ -1,4 +1,6 @@
 import {LikesForPostsRepository} from "../repositories/likes-for-posts-repository";
+import {LikeToPostModel} from "../db/schemas";
+import {LikeStatus} from "../models/common";
 
 
 export class LikeForPostsService {
@@ -9,5 +11,24 @@ export class LikeForPostsService {
 
     static async getDislikes(postId: string) {
         return await LikesForPostsRepository.getNumberOfDisLikes(postId)
+    }
+
+    static async getUsersWhoLikes(postId: string) {
+
+       try {
+            const likes =
+                await LikeToPostModel.find({ postId: postId, status: LikeStatus.LIKE})
+                .sort({ updated: -1 }) // Sort by updated date in descending order
+                .limit(3); // Limit to the latest 3 likes
+            console.log("likes====", likes)
+           return likes.map(like => ({
+                addedAt: like.updated.toISOString(),
+                userId: like.userId,
+                login: like.userLogin
+            }));
+        } catch (error) {
+            console.error('Error fetching latest likes:', error);
+            return [];
+        }
     }
 }
--- src/repositories/posts-query-repository.ts
+++ src/repositories/posts-query-repository.ts
@@ -5,6 +5,8 @@ import {ObjectId, WithId} from "mongodb";
 import {PostMongoModel} from "../db/schemas";
 import {LikeForPostsService} from "../domain/like-for-posts-service";
 import {LikesForPostsRepository} from "./likes-for-posts-repository";
+import {logoutTokenInCookie} from "../middleware/verify-token-in-cookie";
+import {blogValidationPostToBlog} from "../validators/blog-validation";
 
 export class PostsQueryRepository {
 
@@ -39,27 +41,40 @@ export class PostsQueryRepository {
         }
     }
 
-    static async findPostById(id: string, userId: *): Promise<OutputPostModel | null> {
+    static async findPostById(postId: string, userId: string): Promise<OutputPostModel | null> {
 
-        if (!ObjectId.isValid(id)) return null
-        const post: WithId<PostDbModel> | null = await PostMongoModel.findById(id)
+        if (!ObjectId.isValid(postId)) return null
+        const post: WithId<PostDbModel> | null = await PostMongoModel.findById(postId)
 
 
-        console.log("find post by id=", post)
+        console.log("find post by postId=", post)
 
-        post!.extendedLikesInfo.likesCount=await LikeForPostsService.getLikes(id)
-        post!.extendedLikesInfo.dislikesCount=await LikeForPostsService.getDislikes(id)
+        post!.extendedLikesInfo.likesCount = await LikeForPostsService.getLikes(postId)
+        post!.extendedLikesInfo.dislikesCount = await LikeForPostsService.getDislikes(postId)
 
-        if (userId == null) {
-            post!.extendedLikesInfo.myStatus="None"
+        console.log("userId=", userId)
+        if (!userId) {
+            post!.extendedLikesInfo.myStatus = "None"
             console.log("post=", post)
             return post ? postMapper(post) : null
         }
-        post!.extendedLikesInfo.myStatus= await LikesForPostsRepository.getMyStatus(id, userId)
+        post!.extendedLikesInfo.myStatus = await LikesForPostsRepository.getMyStatus(postId, userId)
 
+        let array =
+            await LikeForPostsService.getUsersWhoLikes(postId)
 
+        console.log("------------array>,", array)
+        post!.extendedLikesInfo.newestLikes = array;
         console.log("post=", post)
-        return post ? postMapper(post) : null
+
+        if (!post) return null
+
+        const afterMapping = postMapper(post)
+
+        afterMapping.extendedLikesInfo.newestLikes= array
+
+        console.log("afterMapping=", afterMapping)
+        return afterMapping
     }
 
 }
